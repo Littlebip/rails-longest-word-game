52caa7f940d24de1debff5ec17120e74c53be57f
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-05-29 14:46:48 +0200

    Completed action relationship

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,40 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    charset = ('A'..'Z').to_a
+    @letters = Array.new(10) { charset.sample }
+  end
+
+  def score
+    @set = params[:array_of_letters].chars
+    @trial = params[:word].upcase.chars
+    if trial_existence_checker(params[:word]) == false
+      @results = "Sorry but #{params[:word]} does not seem to be a valid English word..."
+    elsif trial_in_set_checker(@trial, @set) == false
+      @results = "Sorry but #{params[:word]} can't be build out of #{params[:array_of_letters]}"
+    else
+      @results = "Congratulations! #{params[:word]} is a valid English word!"
+    end
+  end
+
+  def trial_in_set_checker(trial, set)
+    trial.each do |letter|
+      if set.include? letter
+        trial.delete_at(trial.find_index(letter))
+        set.delete_at(set.find_index(letter))
+      else
+        return false
+      end
+    end
+    true
+  end
+
+  def trial_existence_checker(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    check_string = URI.open(url).read
+    check_hash = JSON.parse(check_string)
+    check_hash['found']
+  end
+end
